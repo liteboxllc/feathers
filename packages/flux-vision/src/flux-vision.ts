a3116303c8220e9ebe436604246bd61285ca5ef5
Steven Merrill <steven.merrill@gmail.com>
2022-07-14 14:58:46 -0400

    Add all the order event variants

--- packages/flux-vision/src/flux-vision.ts
+++ packages/flux-vision/src/flux-vision.ts
@@ -1,38 +1,35 @@
 interface ProductData {
-  quantity: Number,
-  price: Number,
-  product_id: String,
-  sku: String,
-  url: String,
-  name: String,
-  category: String,
+  quantity: number;
+  price: number;
+  product_id: string;
+  sku: string;
+  url: string;
+  name: string;
+  category: string;
 }
 
-/* eslint-disable @typescript-eslint/camelcase */
 export default class FluxVision {
   htmlDataElements: string;
   checkoutDataset: DOMStringMap;
   productData: ProductData[];
-  Shopify: { Checkout: { page: unknown; step: unknown; isOrderStatus: boolean } };
-  firstOrderStatusView: boolean;
+  Shopify: {
+    Checkout: { page: unknown; step: unknown; isOrderStatus: boolean };
+  };
   analytics: SegmentAnalytics.AnalyticsJS;
   liquidDivSelector: string;
 
   constructor({
     analytics,
     Shopify,
-    firstOrderStatusView = false,
     liquidDivSelector = "#FLUX_VISION_DATASETS",
   }) {
     this.productData = [];
     this.analytics = analytics;
     this.Shopify = Shopify;
-    this.firstOrderStatusView = firstOrderStatusView;
     this.liquidDivSelector = liquidDivSelector;
   }
 
-  public init(firstOrderStatusView: boolean = false): void {
-    this.firstOrderStatusView = firstOrderStatusView;
+  public init(): void {
     this.checkDomForSelector();
     this.pullDataFromDOM();
     this.sendAnalytics();
@@ -43,7 +40,7 @@ export default class FluxVision {
     const liquidElement = document.querySelector(liquidDivSelector);
     if (!liquidElement) {
       throw new Error(
-        `No liquid element found with selector ${liquidDivSelector}. Learn more at https://github.com/broadlume/feathers/tree/master/packages/flux-vision`,
+        `No liquid element found with selector ${liquidDivSelector}. Learn more at https://github.com/broadlume/feathers/tree/master/packages/flux-vision`
       );
     }
   }
@@ -53,13 +50,13 @@ export default class FluxVision {
 
     //checkout data
     const checkoutElemement = document.querySelector<HTMLDataElement>(
-      "#checkout-data",
+      "#checkout-data"
     );
     this.checkoutDataset = Object.assign({}, checkoutElemement.dataset);
 
     // product data
     const productsDatasets = document.querySelectorAll<HTMLDataElement>(
-      "#product-item-for-analytics-dataset",
+      "#product-item-for-analytics-dataset"
     );
 
     for (let i = 0; i < productsDatasets.length; i++) {
@@ -67,7 +64,6 @@ export default class FluxVision {
       if (productDataset) {
         const formattedPrice = Number(productDataset.price) / 100;
         productDataset.product_id = productDataset.name;
-        const objectProduct = Object.assign({}, productDataset);
         productData.push({
           quantity: Number(productDataset.quantity),
           price: formattedPrice,
@@ -83,7 +79,11 @@ export default class FluxVision {
 
   private sendAnalytics(): void {
     const { analytics, checkoutDataset, productData } = this;
-    const { currentStep, currentPage, isOrderStatus } = this.getCurrentEnvironment();
+    const {
+      currentStep,
+      currentPage,
+      isOrderStatus,
+    } = this.getCurrentEnvironment();
 
     switch (currentStep) {
       case "contact_information":
@@ -115,26 +115,46 @@ export default class FluxVision {
         break;
     }
 
-    if (currentPage == "thank_you" || (isOrderStatus === true || this.firstOrderStatusView == true)) {
-      analytics.track("Order Completed", {
-        checkout_id: checkoutDataset.checkoutId,
-        order_id: checkoutDataset.orderNumber,
-        total: Number(checkoutDataset.totalPrice) / 100,
-        revenue: Number(checkoutDataset.totalPrice) / 100,
-        currency: "USD",
-        products: productData,
-        source: "website",
-      });
-
-      if (productData.filter(a => a.sku.startsWith("SI-LB")).length > 0) {
-        analytics.track("Liteboxer Ordered", {
+    if (currentStep == "thank_you") {
+      const trackOrder = (event: string) => {
+        analytics.track(event, {
           checkout_id: checkoutDataset.checkoutId,
-          order_id: checkoutDataset.orderNumber,
+          order_id: checkoutDataset.orderId,
           total: Number(checkoutDataset.totalPrice) / 100,
           currency: "USD",
           products: productData,
           source: "website",
         });
+      };
+      trackOrder("Order Completed");
+
+      const orderHasLiteboxer =
+        productData.filter(
+          (i) => i.sku.startsWith("SI-LB") && !i.sku.startsWith("SI-LBGO")
+        ).length > 0;
+      const orderHasWallMount =
+        productData.filter((i) => i.sku.startsWith("SI-LBWM")).length > 0;
+      const orderHasFloorStand =
+        productData.filter(
+          (i) =>
+            i.sku.startsWith("SI-LB") &&
+            !i.sku.startsWith("SI-LBGO") &&
+            !i.sku.startsWith("SI-LBWM")
+        ).length > 0;
+      const orderHasGo =
+        productData.filter((i) => i.sku.startsWith("SI-LBGO")).length > 0;
+
+      if (orderHasLiteboxer) {
+        trackOrder("Liteboxer Ordered");
+      }
+      if (orderHasWallMount) {
+        trackOrder("Wall Mount Ordered");
+      }
+      if (orderHasFloorStand) {
+        trackOrder("Floor Stand Ordered");
+      }
+      if (orderHasGo) {
+        trackOrder("Go Ordered");
       }
     }
   }
@@ -149,7 +169,7 @@ export default class FluxVision {
     return {
       currentStep: Shopify.Checkout.step,
       currentPage: Shopify.Checkout.page,
-      isOrderStatus: Shopify.Checkout.isOrderStatus
+      isOrderStatus: Shopify.Checkout.isOrderStatus,
     };
   }
 }
